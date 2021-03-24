26af3da4618ab2fdf8f311b05bb4bd06f8871b08
Daniele <bambudio@libero.it>
2021-03-24 15:08:37 +0100

    Esercizio completo

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ var app = new Vue (
   {
     el: '#root',
     data: {
-
+      titolo: 'Vue Js',
+      image: 'https://www.laramind.com/blog/wp-content/uploads/2018/03/Untitled-design-16-620x420.png',
+      descrizione: 'The new Framework'
     }
   }
 )
